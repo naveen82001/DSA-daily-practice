e57c43b1602b1a3aa6ab288631a6db9a3de813c3
Aditya <saiaditya3042off@gmail.com>
2022-02-03 09:55:46 +0530

    stock span done

---
+++ Stack/StockSpan.cpp
@@ -0,0 +1,45 @@
+// Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+// The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backward) for which the stock price was less than or equal to today's price.
+
+// For example, if the price of a stock over the next 7 days were [100,80,60,70,60,75,85], then the stock spans would be [1,1,1,2,1,4,6].
+// Implement the StockSpanner class:
+
+// StockSpanner() Initializes the object of the class.
+// int next(int price) Returns the span of the stock's price given that today's price is price.
+
+// Input
+// ["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+// [[], [100], [80], [60], [70], [60], [75], [85]]
+// Output
+// [null, 1, 1, 1, 2, 1, 4, 6]
+
+#include <bits/stdc++.h>
+
+class StockSpanner
+{
+public:
+    stack<pair<int, int>> st;
+    StockSpanner()
+    {
+    }
+
+    int next(int price)
+    {
+        int ans = 1;
+
+        while (!st.empty() and st.top().first <= price)
+        {
+            ans += st.top().second;
+            st.pop();
+        }
+        st.push({price, ans});
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
